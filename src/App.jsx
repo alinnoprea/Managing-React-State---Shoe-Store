cd740384c5feaca20553b4f4ba40282ea668467b
alinnoprea <opreaalin2001@gmail.com>
2023-05-29 19:53:58 +0300

    Implementing Form Validation

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Products from "./Products";
 import { Routes, Route } from "react-router-dom";
 import Detail from "./Detail";
 import Cart from "./Cart";
+import Checkout from "./Checkout";
 
 export default function App() {
   const [cart, setCart] = useState(() => {
@@ -38,6 +39,11 @@ export default function App() {
         : items.map((i) => (i.sku === sku ? { ...i, quantity } : i));
     });
   }
+
+  function emptyCart() {
+    setCart([]);
+  }
+
   return (
     <>
       <div className="content">
@@ -55,7 +61,17 @@ export default function App() {
             />
             <Route
               path="/cart"
-              element={<Cart cart={cart} updateQuantity={updateQuantity} />}
+              element={
+                <Cart
+                  cart={cart}
+                  updateQuantity={updateQuantity}
+                  emptyCart={emptyCart}
+                />
+              }
+            />
+            <Route
+              path="/checkout"
+              element={<Checkout cart={cart} emptyCart={emptyCart} />}
             />
           </Routes>
         </main>
